cbdcbe46216a546e4e6e77b935fa8df2000fa616
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-01-26 15:48:36 +0600

    26.01.2022

--- Day/ConvertArrayOfStringToNumbers.js
+++ Day/ConvertArrayOfStringToNumbers.js
@@ -3,6 +3,7 @@
 console.log(arrayOfStringToNumber(['2', '3', '4'])); */
 
 //way 2
-const arrayOfStringToNumber = (arr) => arr.map((i)=>console.log(`without convert: ${typeof(i)}, with convert: ${typeof(+i)}, main item: ${i}`));
+const arrayOfStringToNumber = (arr) => arr.map((i) => console.log(`without convert: ${typeof (i)}, with convert: ${typeof (+i)}, main item: ${i}`));
+Note:"When use plus operator (+) before value then vule convert a Number"
 arrayOfStringToNumber(['2', '3', '4']);
 
