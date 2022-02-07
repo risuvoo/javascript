5bc20aab0fa22889590334b01f59fcc4a13563f3
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-02-07 12:45:44 +0600

    loop

---
+++ HackerRank/daythree.js
@@ -0,0 +1,12 @@
+function vowelsAndConsonants(s) {
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u') {
+            console.log(s[i])
+        }
+    }
+     for (var j = 0; j < s.length; j++) {
+         if (s[j] !== 'a' && s[j] !== 'e' && s[j] !== 'i' && s[j] !== 'o' && s[j] !== 'u') { console.log(s[j]) }
+    }
+    
+}
+vowelsAndConsonants('javascriptloops');
